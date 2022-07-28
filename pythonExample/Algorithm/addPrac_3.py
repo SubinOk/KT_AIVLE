090fb1ad669ab7d64bc21c9df94e6f8c772aafd6
egoing <egoing@a.com>
2022-07-28 16:36:28 +0900

    오탈자 수정

--- pythonExample/Algorithm/addPrac_3.py
+++ pythonExample/Algorithm/addPrac_3.py
@@ -5,13 +5,13 @@
 def solution(left, right):
     answer = 0
     for i in range(left, right+1):
-        if findDivOdd(i):
+        if findDivEven(i):
             answer += i
         else:
             answer -= i
     return answer
 
-def findDivOdd(n):
+def findDivEven(n):
     count = 0
     for i in range(1, n+1):
         if not n%i:
